932e57f444d82977c82919d5141e91843a73df39
Junior Oliveira <junior.getsoftware@gmail.com>
2018-05-26 23:25:58 -0300

    criando funções e manipulações simples

---
+++ Manipulação com js/main.js
@@ -0,0 +1,26 @@
+function somar(){
+    num1 = pegarValorNumerico('num1');
+    num2 = pegarValorNumerico('num2');
+    res = num1 + num2;
+    escrever('resultado', res);
+}
+
+function isNumero(num){
+    return typeof num === 'number';
+}
+
+function pegarValor(id){
+    return document.getElementById(id).value;
+}
+
+function toFloat(num){
+    return parseFloat(num);
+}
+
+function pegarValorNumerico(num){
+    return toFloat(pegarValor(num));
+}
+
+function escrever(onde, valor){
+    document.getElementById(onde).innerHTML = valor;
+}
